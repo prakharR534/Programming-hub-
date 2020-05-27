9a973240d044f6f0de1cd3c42e2b48cbe8b2e6fe
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-05-27 21:17:42 +0530

    Add files via upload

---
+++ Checking if a number is a power of two or not.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isPowerofTwo(int n){
+	return (ceil(log2(n)) == floor(log2(n)));
+}
+
+//only for integer values
+
+int main(){
+	int n;
+	cin>>n;
+	
+	if( isPowerofTwo(n)){
+		cout<<"YES";
+	}
+	else
+	cout<<"NO";
+}
