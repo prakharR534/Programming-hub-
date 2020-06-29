65ef701e4e5d3e2582a0058d7e2422a8af6088f3
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-29 18:21:55 +0530

    Create Smaller Greater Equal Numbers | PrepBytes.cpp

---
+++ Smaller Greater Equal Numbers | PrepBytes.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t--){
+   long long int n,k,e=0,l=0,m=0;
+    cin>>n>>k;
+    int A[n];
+    
+    for(int i=0;i<n;i++)
+    cin>>A[i];
+    
+    
+    for(int i=0;i<n;i++)
+    {
+      if(A[i]>k){
+        m++;
+      }
+      else if(A[i]<k){
+        l++;
+      }
+      else if(A[i] == k){
+        e++;
+      }
+    }
+      
+      
+      cout<<l<<" "<<m<<" "<<e<<endl;
+    
+    
+  }
+  
+  return 0;
+}
