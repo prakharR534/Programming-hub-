f987be0a636e53c347f6fb6824fa93b62bb3f470
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-07-06 00:25:30 +0530

    Create Part A: House Hunting | PrepBytes.cpp

---
+++ Part A: House Hunting | PrepBytes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  float annual_salary, total_cost ,portion_saved;
+  cin>>annual_salary>>total_cost>>portion_saved;
+  int count=0;
+  
+float portion_down_payment = total_cost *0.25;
+
+float current_savings=0;
+
+
+while(current_savings < portion_down_payment){
+  
+  current_savings += current_savings * 0.04 / 12  + annual_salary*portion_saved/12;
+  count++;
+}
+  
+  
+  cout<<count;
+  return 0;
+}
